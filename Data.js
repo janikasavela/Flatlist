69847fd0b8b8b71e9e45d087738dc69a7d074bf1
Janika Savela <janika_s@hotmail.com>
2023-10-09 09:44:00 +0300

    eka

---
+++ Data.js
@@ -0,0 +1,76 @@
+export const DATA = [
+{ id: '1', 
+  lastname: 'Hyvärinen',
+  firstname: 'Miia'
+},
+{
+    id: '2',
+    lastname: 'Savela',
+    firstname: 'Janika'
+},
+{
+    id: '3',
+    lastname: 'Turunen',
+    firstname: 'Iina'
+},
+{
+    id: '4',
+    lastname: 'Raudaskoski',
+    firstname: 'Rosa'
+},
+{
+    id: '5',
+    lastname: 'Oikarinen',
+    firstname: 'Anni'
+},
+{
+    id: '6',
+    lastname: 'Rautio',
+    firstname: 'Maiju'
+},
+{
+    id: '7',
+    lastname: 'Harju',
+    firstname: 'Pirkko'
+},
+{
+    id: '8',
+    lastname: 'Pihlajaniemi',
+    firstname: 'Sari'
+},
+{
+    id: '9',
+    lastname: 'Rantaiso',
+    firstname: 'Anna-Mari'
+},
+{
+    id: '10',
+    lastname: 'Koskela',
+    firstname: 'Tarja'
+},
+{
+    id: '11',
+    lastname: 'Kukkola',
+    firstname: 'Airi'
+},
+{
+    id: '12',
+    lastname: 'Häkkilä',
+    firstname: 'Maarit'
+},
+{
+    id: '13',
+    lastname: 'Lundberg',
+    firstname: 'Tuija'
+},
+{
+    id: '14',
+    lastname: 'Suorsa',
+    firstname: 'Marika'
+},
+{
+    id: '15',
+    lastname: 'Huttunen',
+    firstname: 'Terttu'
+}
+]
